aa34708c4dbfd285a13943863b9c2485ee130845
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2024-11-16 04:54:47 +0100

    view and logic of an item info edition --> (preliminar version)

--- frontend/src/app/components/item-modification/item-modification.component.ts
+++ frontend/src/app/components/item-modification/item-modification.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ItemService } from '../../services/item.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-item-modification',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './item-modification.component.html',
   styleUrl: './item-modification.component.css'
 })
@@ -14,14 +15,25 @@ export class ItemModificationComponent {
 
   id: any;
   item: any;
+  sizes = [{value: 'S', selected: false},{value: 'M', selected: false},{value: 'L', selected: false},{value: 'XL', selected: false}];
+
+  @ViewChild('imageFile')
+  imageFile: any;
 
   constructor(public router: Router, public activatedRoute: ActivatedRoute, public itemService: ItemService){}
 
   ngOnInit(){
     this.id = this.activatedRoute.snapshot.params['id'];
     this.itemService.getItem(this.id).subscribe(
-      item => this.item = item.content,
-      error => console.log(error)
+      item => {
+        this.item = item.content;
+        for(var i=0; i<item.content.sizes.length; i++){
+          if(item.content.sizes[i] != null){
+            this.sizes[i].selected = true;
+          }
+        }
+      },
+      error => console.log(error)     
     );
   }
 
