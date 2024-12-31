42fe13cc2154f87e7c39e6f3677a233e196cf65a
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2024-12-31 04:44:54 +0100

    registration working

--- backend/src/main/java/es/webapp/webapp/controller/UserController.java
+++ backend/src/main/java/es/webapp/webapp/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.webapp.webapp.model.Direction;
+import es.webapp.webapp.model.ShoppingCart;
 import es.webapp.webapp.model.User;
 import es.webapp.webapp.service.UserService;
 
@@ -45,34 +46,19 @@ public class UserController {
     @PostMapping("/new")
     public String newUser(Model model, User user, Direction address, MultipartFile imageField, @RequestParam String passwordConfirmation) throws IOException{
 
+        user.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
+ 
 
-        if(imageField.isEmpty()){
-            Optional<User> anonymous = userService.findById(59);
-            if(anonymous.isPresent()) {
-                user.setImageFile(anonymous.get().getImageFile());
-            }
-        }else{
-            user.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
-        }    
-
-        if(user.getPassword().equals(passwordConfirmation)){
-            if(passwordConfirmation.length() < 3 && passwordConfirmation.length() > 10){
-                model.addAttribute("size", "password must contains between 6 and 10 characters");
-            } else {
-                model.addAttribute("size", "");
-            } 
-            if(address.getZipCode().toString().length() != 5){
-                model.addAttribute("zip_code", "zip code must contains 5 characters");
-            } else {
-                model.addAttribute("zip_code", "");
-            }              
-                user.setRol("USER");
-                user.setDirection(address);
-                userService.add(user);
-                model.addAttribute("state_reg", "user registered");
+        if(user.getPassword().equals(passwordConfirmation)){            
+            user.setRol("USER");
+            ShoppingCart cart = new ShoppingCart();
+            user.setDirection(address);
+            user.setShoppingCart(cart);
+            userService.add(user);
+            model.addAttribute("state_reg", "user registered");
          
         }else{
-            model.addAttribute("state_reg", "some mnadatory fields are empty or incorrect");
+            model.addAttribute("state_reg", "password is out of range [5-10] or not correspond with the confirmation");
         }
         model.addAttribute("state_log", "");
 
