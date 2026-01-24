53c149e1a0f7603d7ddae19ca8dbec87078b92b0
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2026-01-24 16:26:26 +0100

    settings for docker deployment

--- backend/src/main/java/es/webapp/webapp/MySQLConfig.java
+++ backend/src/main/java/es/webapp/webapp/MySQLConfig.java
@@ -1,6 +1,6 @@
 package es.webapp.webapp;
 
-import java.util.HashMap;
+/*import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -31,7 +31,7 @@ public class MySQLConfig {
     public DataSource itemDataSource(){
         return DataSourceBuilder.create().driverClassName("com.mysql.cj.jdbc.Driver")
                                                         //.url("jdbc:mysql://mysqldb2:3306/items")
-                                                        .url("jdbc:mysql://localhost:3306/items")
+                                                        .url("jdbc:mysql://db:3306/items")
                                                         .username("root")
                                                         .password("Mundialmente1")
                                                         .build();
@@ -64,4 +64,4 @@ public class MySQLConfig {
 
         return transactionManager;
     }
-}
+}*/
